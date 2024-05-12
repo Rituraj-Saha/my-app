43c221fcedbeaa6f4be5d1dad224d67964015d3f
Rituraj Saha <rituraj10saha@gmail.com>
2024-05-13 01:49:05 +0530

    change contact

--- src/coponents/ContactUs/Contact.jsx
+++ src/coponents/ContactUs/Contact.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 import { Element } from "react-scroll";
-import "./Contact";
+import "../ContactUs/Contact.css";
+import Divider from "@mui/material/Divider";
+import TextField from "@mui/material/TextField";
+import Textarea from "@mui/joy/Textarea";
+import Button from "@mui/material/Button";
+import useIsMobile from "../../util/useIsMobile";
+
 const Contact = () => {
   return (
     <Element name="contact" className="contact" id="contact">
       {" "}
-      <div>
+      <div className="contactHeading">
+        <span>Contact Us</span>
+      </div>
+      <div
+        className={useIsMobile() ? "contact-parent-mobile" : "contact-parent"}
+      >
         <div className="left">
           <div className="addresss-tag">address</div>
           <div className="phone-tag">+91 9674345373</div>
           <div className="email">Homecrafter@gmail.com</div>
         </div>
-        <div className="right"></div>
+        <Divider orientation="vertical" flexItem />
+        <div className="right">
+          <div>
+            <span>Send Us a Message</span>
+          </div>
+          <TextField id="outlined-basic" label="Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basic" label="Phone" variant="outlined" />
+          <Textarea placeholder="Type your message" minRows={2} />
+          <Button variant="contained">Send</Button>
+        </div>
       </div>
     </Element>
   );
